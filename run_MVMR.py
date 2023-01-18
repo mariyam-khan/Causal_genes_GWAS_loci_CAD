df9d2a5a9357970a6a0f68170c3239a81841d066
mariyam-khan <35807706+mariyam-khan@users.noreply.github.com>
2023-01-18 16:33:57 +0100

    Add files via upload
    This is the file for getting the causal genes for cases the user can supply the LD-matrix. This file can be downloaded and run on the terminal as :
    python3 "/home/user/exposure_outcome.csv" "/home/user/ld.csv"
    After python3, the first argument should be the exposure_outcome.csv file containing the SNPs to exposure effects and SNPs to outcome effects. The first column is always the SNP id's (the ID is irrelevant if you are providing the LD-matrix but should still be filled with default values), the last column is always the SNPs to outcome effect. Everything column in between is treated as an exposure. The separator to be used is comma. As an example:
    
    SNPs,AS3MT,SFXN2,CAD
    rs11191416,-0.5,0.37,0.079
    rs7098825,-0.34,0.0,0.078
    rs17115100,-0.4,0.540.05
    
    The second argument is ld.csv file for the LD matrix. Please make sure the ordering of the SNPs is same as the exposure_outcome file. This has the format:
    1 0.98852 0.96565 0.98
    0.98852 1 0.965852 0.994254 
    0.96565 0.965852 1 
    
    The delimiter is space. 
    The results are saved as a .csv file in the same directory as the one given for the exposure_outcome.csv file. 
    The code automatically prunes for SNPs in perfect-LD and keeps only the first occurring SNPs. If you would like to keep the most significant SNPs amongst perfect LD SNPs then please order the SNPs in decreasing order of significance in both .csv files.

---
+++ run_MVMR.py
@@ -0,0 +1,60 @@
+from scipy.linalg import lstsq
+from numpy import *
+import numpy as np
+import pandas as pd
+import sys
+
+file_EXEY = sys.argv[1]
+file_EE = sys.argv[2]
+Data = pd.read_csv(file_EXEY, sep=',')
+cov_EE = np.genfromtxt(file_EE, delimiter=' ')
+for (columnName, columnData) in Data.iteritems():
+    if (Data[columnName] == 0).all():
+        Data.drop(columnName, axis=1, inplace=True)
+snps = Data.loc[:, 'SNPs'].values
+if len(snps) != 1:
+    cov_data = pd.DataFrame(cov_EE, columns=snps, index=snps, dtype='float')
+    upper_tri = cov_data.where(np.triu(np.ones(cov_data.shape), k=1).astype(bool))
+    to_drop = [column for column in upper_tri.columns if any(upper_tri[column] == 1.0)]
+    cov_data = cov_data.drop(labels=to_drop, axis=1)
+    cov_data = cov_data.drop(labels=to_drop, axis=0)
+    Data = Data[Data.SNPs.isin(cov_data.index.values)]
+    Data.reset_index(drop=True, inplace=True)
+    cov_EE = cov_data.values
+    snps_new = Data.loc[:, 'SNPs'].values
+    no_snps = len(snps_new)
+    no_genes = len(Data.columns) - 2
+    df = Data.loc[:, ~Data.columns.isin(['SNPs', 'CAD'])]
+    gene_names = column_names = list(df.columns.values)
+    covEY = Data.loc[:, 'CAD'].values
+    covEX = df.values
+    if no_snps == no_genes:
+        if no_snps == 1:
+            b_est = covEY / covEX
+            d1 = {'gene': gene_names,
+                  'Causal Estimate Ratio method': b_est}
+            df1 = pd.DataFrame(data=d1)
+        else:
+            b_est = np.linalg.solve(covEX, covEY)
+            d1 = {'gene': gene_names,
+                  'Causal Estimate': b_est}
+            df1 = pd.DataFrame(data=d1)
+    else:
+        b_est2, res, rnk, sy = lstsq(covEX, covEY)
+        b_est1 = np.linalg.inv(covEX.T @ np.linalg.inv(cov_EE) @ covEX) @ (covEX.T @ np.linalg.inv(cov_EE) @ covEY)
+        d1 = {'gene': gene_names,
+              'Causal Estimate Least Squares': b_est2, 'Causal Estimate GMM': b_est1}
+        df1 = pd.DataFrame(data=d1)
+    df1 = df1.set_index('gene')
+else:
+    df = Data.loc[:, ~Data.columns.isin(['SNPs', 'CAD'])]
+
+    gene_names = column_names = list(df.columns.values)
+    covEY = Data.loc[:, 'CAD'].values
+    covEX = df.values
+    b_est = covEY / covEX
+    d1 = {'gene': gene_names,
+          'Causal Estimate Ratio method': b_est[0]}
+    df1 = pd.DataFrame(data=d1)
+
+df1.to_csv(file_EXEY + "_results.csv", sep=",", float_format='%g')
