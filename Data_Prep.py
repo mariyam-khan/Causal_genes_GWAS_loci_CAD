ec0e2ab162630816a4af5af687aa69f48149a736
mariyam-khan <35807706+mariyam-khan@users.noreply.github.com>
2023-10-27 14:10:42 +0200

    Add files via upload

--- Data_Prep.py
+++ Data_Prep.py
@@ -51,45 +51,41 @@ path = os.path.dirname(file_EX)
 name_EX = os.path.splitext(os.path.basename(file_EX))[0]
 name_EY = os.path.splitext(os.path.basename(file_EY))[0]
 
-Data_out = pd.read_csv(file_EY, sep=',')
-Data_exp = pd.read_csv(file_EX, sep=',')
+Data_out = pd.read_csv(file_EY, sep=',', low_memory=False)
+Data_exp = pd.read_csv(file_EX, sep=',', low_memory=False)
 Data_out = Data_out.sort_values(by='pval.outcome', ascending=True)
 Data_out.reset_index(drop=True, inplace=True)
+
 Data_exp = Data_exp.loc[Data_exp['SNP'].isin(np.intersect1d(Data_exp.SNP, Data_out.SNP))]
 Data_out = Data_out.loc[Data_out['SNP'].isin(np.intersect1d(Data_out.SNP, Data_exp.SNP))]
+
+
 Data_exp.reset_index(drop=True, inplace=True)
 Data_out.reset_index(drop=True, inplace=True)
+
 unique_genes = Counter(Data_exp['exposure']).keys()
 no_genes = len(unique_genes)
 most_common = Counter(Data_exp['exposure']).most_common(no_genes)
-selected_columns = Data_exp['effect_allele.exposure']
-new_df = selected_columns.copy()
-o = Data_out['SNP'].values
-for m in range(len(Data_out['SNP'].values)):
-    value = o[m]
-    idx = Data_exp[Data_exp['SNP'] == value].index.values
-    string_allele = new_df[idx[0]]
-    string_allele = string_allele.replace("'", "")
-    a = Data_out.loc[m, 'effect_allele.outcome']
-    b = Data_out.loc[m, 'other_allele.outcome']
-    if string_allele != Data_out.loc[m, 'effect_allele.outcome']:
-        Data_out.loc[m, 'beta.outcome'] = - Data_out.loc[m, 'beta.outcome']
-        Data_out.loc[m, 'effect_allele.outcome'] = b
-        Data_out.loc[m, 'other_allele.outcome'] = a
-
-        Data_out.loc[m, 'SNP'] = value + "_" + b + "_" + a
-    else:
-        Data_out.loc[m, 'SNP'] = value + "_" + a + "_" + b
 genes = []
 for j in range(no_genes):
     genes.append(most_common[j][0])
-selected_columns2 = Data_out['beta.outcome']
-new_df2 = selected_columns2.copy()
 column_names = genes
-row_names = list(Data_out.SNP)
+row_names = []
+snp_values = list(Data_out.SNP)
+eff_values = Data_out['effect_allele.outcome'].tolist()
+other_values = Data_out['other_allele.outcome'].tolist()
+# Loop through the lists and append elements together with underscores
+for i in range(len(snp_values)):
+    combined_element = f'{snp_values[i]}_{eff_values[i]}_{other_values[i]}'  # Using an f-string to concatenate the elements
+    row_names.append(combined_element)
+
 matrix = np.zeros((len(row_names), len(column_names)))
+
 R_data = pd.DataFrame(matrix, columns=column_names, index=row_names, dtype='object')
 s_data = pd.DataFrame(np.zeros(len(row_names)), columns=['outcome'], index=row_names, dtype='object')
+
+selected_columns = Data_out['effect_allele.outcome']
+new_df = selected_columns.copy()
 
 for n in range(no_genes):
     name = genes[n]
@@ -98,15 +94,28 @@ for n in range(no_genes):
     Data.reset_index(drop=True, inplace=True)
     o = Data['SNP'].values
     for j1 in range(int(most_common[n][1])):
-        value = o[j1] + "_" + Data.loc[j1, 'effect_allele.exposure'] + "_" + Data.loc[j1, 'other_allele.exposure']
-        R_data.at[value, name] = Data.loc[j1, 'beta.exposure']
+        value = o[j1]
         idx = Data_out[Data_out['SNP'] == value].index.values
-        s_data.loc[value, 'outcome'] = new_df2[idx[0]]
+        string_allele = new_df[idx[0]]
+        string_allele = string_allele.replace("'", "")
+        a = Data.at[j1, 'effect_allele.exposure']
+        b = Data.at[j1, 'other_allele.exposure']
+        if string_allele != Data.at[j1, 'effect_allele.exposure']:
+            Data.at[j1, 'beta.exposure'] = - Data.at[j1, 'beta.exposure']
+            Data.at[j1, 'effect_allele.exposure'] = b
+            Data.at[j1, 'other_allele.exposure'] = a
+        value_alleles = o[j1] + "_" + Data_out.at[idx[0], 'effect_allele.outcome'] + "_" + Data_out.at[
+            idx[0], 'other_allele.outcome']
+        R_data.at[value_alleles, name] = Data.at[j1, 'beta.exposure']
+        s_data.at[value_alleles, 'outcome'] = Data_out.at[idx[0], 'beta.outcome']
 
-R_data.replace(np.nan, 0)
-s_data.replace(np.nan, 0)
+R_data = R_data.replace(np.nan, 0)
+s_data = s_data.replace(np.nan, 0)
 df_R = pd.DataFrame(data=R_data)
 df_s = pd.DataFrame(data=s_data)
 RandS = pd.concat([df_R, df_s], axis=1)
 RandS.index.name = 'SNPs'
+
 RandS.to_csv(path + "/" + name_EX + "_prepared.csv", sep=",")
+print("Prepared Datasets have been saved in the directory " + path)
+
